075cc2b4e75e0098c86cbb431b3aca6ef8b975c1
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-02-22 23:06:34 +0530

    Time: 154 ms (38.00%), Space: 130.5 MB (5.08%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,14 +1,55 @@
 class Solution {
+    private:
+    bool knows(vector<vector<int>> &v,int a,int b){
+        if(v[a][b]==1){
+            return true;
+        }
+        return false;
+    }
 public:
     int findJudge(int N, vector<vector<int>>& trust) {
-		vector<int> in(N + 1), out(N + 1);
-		for (auto a : trust) {
-			++out[a[0]];
-			++in[a[1]];
-		}
-		for (int i = 1; i <= N; ++i) {
-			if (in[i] == N - 1 && out[i] == 0) return i;
-		}
-		return -1;
+    vector<vector<int>> v(N+1,vector<int>(N+1,0));
+
+    for(auto i:trust){
+        v[i[0]][i[1]]=1;
     }
+    stack <int> s;
+
+    for(int i=1;i<N+1;i++){
+        s.push(i);
+    }
+
+    while(s.size()>1){
+        int a=s.top();
+        s.pop();
+        int b=s.top();
+        s.pop();
+
+        if(knows(v,a,b)){
+            s.push(b);
+        }
+        else{
+            s.push(a);
+        }
+    }
+    int ans=s.top();
+    s.pop();
+
+    for(int i=1;i<N+1;i++){
+        if(i==ans){
+            continue;
+        }
+        if(v[i][ans]!=1){
+            return -1;
+        }
+    }
+    for(int i=1;i<N+1;i++){
+        if(v[ans][i]!=0){
+            return -1;
+        }
+    }
+    return ans;
+
+
+    }    
 };
