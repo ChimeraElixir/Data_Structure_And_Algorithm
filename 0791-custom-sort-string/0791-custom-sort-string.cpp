397e94dba282d3e2f633d2a214cb4281f819e893
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-03-11 23:21:32 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (83.77%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,26 +1,12 @@
+    string str;
+    bool comparetor(char c1,char c2){
+        return str.find(c1)<str.find(c2);
+    }
 class Solution {
 public:
     string customSortString(string order, string s) {
-        vector<int> frequency(26,0);
-        for(auto i:s){
-            frequency[i-'a']++;
-        }
-        string ans="";
-        for(auto i:order){
-            for( int j=0;j<frequency[i-'a'];j++){
-                ans+=i;
-            }
-            frequency[i-'a']=0;
-        }
-        for(int i=0;i<26;i++){
-            if(frequency[i]!=0){
-                for( int j=0;j<frequency[i];j++){
-                    ans+=char(i+'a');
-                }
-                
-            }
-            
-        }
-        return ans;
+        str=order;
+        sort(s.begin(),s.end(),comparetor);
+        return s;
     }
 };
