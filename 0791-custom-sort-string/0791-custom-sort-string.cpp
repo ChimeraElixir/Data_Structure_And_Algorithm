f30c2653d19c15d1afe321172e7f1762c9c97133
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-03-12 00:00:26 +0530

    Time: 2 ms (53.95%), Space: 8.7 MB (17.36%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,12 +1,9 @@
-    string str;
-    bool comparetor(char c1,char c2){
-        return str.find(c1)<str.find(c2);
-    }
 class Solution {
 public:
     string customSortString(string order, string s) {
-        str=order;
-        sort(s.begin(),s.end(),comparetor);
+        sort(s.begin(),s.end(),[order](char c1,char c2){
+            return order.find(c1)<order.find(c2);
+        });
         return s;
     }
 };
