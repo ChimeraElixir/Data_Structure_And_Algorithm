84095d9c15c245402fc4534c0f213fc5ca5c0af8
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-03-20 17:57:38 +0530

    Time: 146 ms (99.50%), Space: 98.2 MB (13.44%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -1,45 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+auto __untie_cin = cin.tie(nullptr);
+auto __unsync_ios_stdio = ios_base::sync_with_stdio(false);
+
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-
-
-        int temp=0;
-        ListNode* temptr=list1;
-        while(temp<a-1){
-            temptr=temptr->next;
-            temp++;
-        }
-
-
-        int temp2=0;
-        ListNode* temptr2=list1;
-        while(temp2<b){
-            temp2++;
-            temptr2=temptr2->next;
-        }
-
-        cout<<temptr->val<<" "<<temptr2->val;
+        ListNode* curr = list1;
         
-
-        ListNode* temptr3=list2;
-
-        while(temptr3->next!=NULL){
-            temptr3=temptr3->next;
+        for (int i = 1; i < a; i++) {
+            curr = curr->next;
         }
 
-        temptr->next=list2;
-        temptr3->next=temptr2->next;
-        
+        for (int i = a; i <= b; i++) {
+            curr->next = curr->next->next;
+        }
+
+        ListNode* tmp = curr->next;
+        curr->next = list2;
+
+        while (list2->next) {
+            list2 = list2->next;
+        }
+
+        list2->next = tmp;
+
         return list1;
     }
 };
