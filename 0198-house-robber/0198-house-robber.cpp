667d486aec9c8c3319b0d28b57d69040505b38a5
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-08-24 02:56:22 +0530

    Time: 4 ms (31.88%), Space: 9.6 MB (9.75%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& nums,vector<int> &dp){
+
+        if(n==0){
+            return nums[n];
+        }
+        if(n<0) return 0;
+
+        if(dp[n]!=-1) return dp[n];
+
+
+        int right = solve(n-2,nums,dp) + nums[n];
+        int left = solve(n-1,nums,dp) ;
+
+        return dp[n] = max(left,right);
+
+    }
+    int rob(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            return max(nums[1],nums[0]);
+        }
+        vector<int> dp(n,-1);
+        return solve(n-1,nums,dp);
+    }
+};
