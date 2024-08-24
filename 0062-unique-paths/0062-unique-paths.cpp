cf8b28e00c493bc0b6332b9f4386c657938da089
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-08-25 04:42:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -40,32 +40,33 @@ public:
         // return solve(m-1,n-1,vis,di,dj,dp);
 
         //tabulation 
-        vector<int> di = {-1,0};
-        vector<int> dj = {0,-1};
-        vector<vector<int>> vis(m,vector<int> (n,0));
-
+        // vector<int> di = {-1,0};
+        // vector<int> dj = {0,-1};
         vector<vector<int>> dp(m,vector<int> (n,0));
         dp[0][0]=1;
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = 1; // Only one way to reach cells in the first column
-        }
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = 1; // Only one way to reach cells in the first row
-        }
+        // for (int i = 1; i < m; i++) {
+        //     dp[i][0] = 1; // Only one way to reach cells in the first column
+        // }
+        // for (int j = 1; j < n; j++) {
+        //     dp[0][j] = 1; // Only one way to reach cells in the first row
+        // }
 
-        for( int i = 1; i < m; i++){
-            for(int j = 1;j < n; j++){
-                int count = 0;
-                for(int k=0;k<2;k++){
-                    int idx = i + di[k];
-                    int jdx = j + dj[k];
+        for( int i = 0; i < m; i++){
+            for(int j = 0;j < n; j++){
+                // int count = 0;
+                // for(int k=0;k<2;k++){
+                    // int idx = i + di[k];
+                    // int jdx = j + dj[k];
+                    // if(i == 0 && j==0) dp[i][j]=1;
+                    if (i>0) dp[i][j]+= dp[i-1][j];
+                    if (j>0) dp[i][j]+= dp[i][j-1];
 
-                    if( idx>=0 && jdx>=0 ){
-                        count+=dp[idx][jdx];
-                    }
+                    // if( idx>0 && jdx>0 ){
+                    //     count+=dp[idx][jdx];
+                    // }
 
-                }
-                dp[i][j]=count;
+                // }
+                // dp[i][j]=count;
             }
         }
         return dp[m-1][n-1];
