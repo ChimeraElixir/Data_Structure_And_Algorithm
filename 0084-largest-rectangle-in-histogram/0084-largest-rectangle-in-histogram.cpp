e9aa491189137b8c2a3c9746fd80625938b68014
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-09-07 10:52:19 +0530

    Time: 106 ms (71.76%), Space: 79.9 MB (74.41%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    vector<int> NSE(vector<int>& arr,int n){
-        stack <int> st;
-        vector<int> ans (n,n);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[i]<=arr[st.top()]){
-                st.pop();
-            }
-            if(!st.empty()){
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> PSE(vector<int>& arr,int n){
-        stack <int> st;
-        vector<int> ans (n,-1);
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[i]<arr[st.top()]){
-                st.pop();
-            }
-            if(!st.empty()){
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
     int largestRectangleArea(vector<int>& arr) {
         int n = arr.size();
-        vector<int> nse = NSE(arr,n);
-        vector<int> pse = PSE(arr,n);
-        int ans = 0;
+        stack <int> st;
+        int maxArea = 0;
         for(int i=0;i<n;i++){
-            ans = max(ans, arr[i] * (nse[i] - pse[i]-1));
+            while(!st.empty() && arr[i]<arr[st.top()]){
+                int element = arr[st.top()];
+                st.pop();
+                int nse = i;
+                int pse = st.empty()?-1:st.top();
+                maxArea = max(maxArea,element*(nse - pse - 1));
+            }
+            st.push(i);
         }
-        return ans;
+        while(!st.empty()){
+            int nse = n;
+            int element = arr[st.top()];
+            st.pop();
+            int pse = st.empty()?-1:st.top();
+            maxArea = max(maxArea,element*(nse - pse - 1));
+        }
+
+        return maxArea;
     }
 };
