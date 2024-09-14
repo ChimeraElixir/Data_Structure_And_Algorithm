6327c5d4fe56e313c447c2e04469228393694522
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-09-15 00:39:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.43%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size() != goal.size()) return false;
-        string check = s+s;
-        return check.find(goal) != string::npos;
+        int len=s.length();
+        for(int i=0;i<len;i++)
+        {
+            s+=s[i];
+            string sub=s.substr(i+1,len+1);
+            if(sub==goal)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
