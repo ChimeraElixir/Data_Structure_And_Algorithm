4188579a25785215274e4fac3f90a69c1a36e041
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-10-16 08:00:15 +0530

    Time: 26 ms (40.71%), Space: 22.9 MB (75.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,31 @@
+bool cmp(vector<int> &a,vector<int> &b){
+    if(a[0]==b[0]){
+        return a[1]<b[1];
+    }
+    return a[0]<b[0];
+}
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        if (n <= 1) return intervals;
-        sort(intervals.begin(),intervals.end());
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end(),cmp);
+        
+        vector<vector<int>> ans;
 
-        vector<vector<int>> result;
-        result.push_back(intervals[0]);
+        ans.push_back(intervals[0]);
 
         for(int i=1;i<n;i++){
+            vector<int> &top = ans.back();
 
-            if(intervals[i][0]<=result.back()[1]){
-                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            if(intervals[i][0]<=top[1]){
+                top[1] = max(intervals[i][1],top[1]);
             }
             else{
-                result.push_back(intervals[i]);
+                ans.push_back(intervals[i]);
             }
         }
-        return result;
-        
+
+        return ans;
+
     }
 };
