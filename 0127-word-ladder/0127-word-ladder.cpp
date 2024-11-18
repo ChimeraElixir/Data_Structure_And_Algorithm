bd42077f0ba512db854e70f29c33f44b8e000997
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-11-18 13:42:11 +0530

    Time: 305 ms (36.27%), Space: 19.9 MB (44.1%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        set<string> s(wordList.begin(),wordList.end());
+        int n = beginWord.size();
+
+        q.push({beginWord,1});
+
+        while(!q.empty()){
+            string word = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+
+            if(word == endWord){
+                return dist;
+            }
+            else{
+                for(int i=0;i<n;i++){
+                    string copy = word;
+                    for(char c = 'a';c<='z';c++){
+                        copy[i] = c;
+                        if(s.find(copy) != s.end()){
+                            s.erase(copy);
+                            q.push({copy,dist+1});
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
