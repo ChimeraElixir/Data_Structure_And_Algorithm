fbab3c0bacf7cd52c242ad83f9b338186872a04b
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-11-22 18:09:13 +0530

    Time: 0 ms (100%), Space: 18.3 MB (93.64%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,3 +1,49 @@
+class DisjointSet{
+    vector<int> parent,size;
+    
+    public:
+        DisjointSet(int n){
+            parent.resize(n);
+            size.resize(n);
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+                size[i] = 1;
+            }
+        }
+        int findParent(int node){
+            if(node == parent[node]){
+                return node;
+            }
+            return parent[node] = findParent(parent[node]);
+            
+        }
+        
+        void unionBySize(int u , int v){
+            int pu = findParent(u);
+            int pv = findParent(v);
+            if(pu == pv) return;
+            
+            if(size[pu]>size[pv]){
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+            else{
+                parent[pu] = pv ; 
+                size[pv] += size[pu];
+            }
+        }
+        int answer(int V){
+            int ans = 0;
+            for(int i=0;i<V;i++){
+                if(parent[i]==i){
+                    ans++;
+                }
+            }
+            return ans;
+        }
+        
+};
+
 class Solution {
 private:
     void dfs(int node,vector<vector<int>>& isConnected,vector<int>& visited,int n){
@@ -13,17 +59,41 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         
-        vector<int> visited(n,0);
+        //******** DisJointSet *********//
 
-        int count = 0;
+        DisjointSet ds(n);
 
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,isConnected,visited,n);
-                count++;
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j] == 1){
+                    ds.unionBySize(i,j);
+                }
             }
-    
         }
-        return count;
+        
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(ds.findParent(i) == i){
+                ans++;
+            }
+        }
+        
+        return ans;
+
+
+        //******** DFS ********//
+
+        // vector<int> visited(n,0);
+
+        // int count = 0;
+
+        // for(int i=0;i<n;i++){
+        //     if(!visited[i]){
+        //         dfs(i,isConnected,visited,n);
+        //         count++;
+        //     }
+    
+        // }
+        // return count;
     }
 };
