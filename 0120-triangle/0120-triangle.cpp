caf606ee27223216d1c3b41b8099373b1a180d38
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2024-11-29 01:48:58 +0530

    Time: 1 ms (21.58%), Space: 13.1 MB (5.71%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
 
     int solve(int row,int col ,vector<vector<int>>& triangle ,vector<vector<int>>&dp){
-        if(col < 0 || col > row) return 1e9;
-        if(row == 0) return triangle[0][0];
+        if(col < 0 || col > row) return 1e9;
+        if(row == 0) return triangle[0][0];
 
         if(dp[row][col]!=-1) return dp[row][col];
 
@@ -15,14 +15,36 @@ public:
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
-        int mini = INT_MAX;
+        // int mini = INT_MAX;
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            int ans = solve(n-1,i,triangle,dp);
-            mini =  min(mini,ans);
-        }
-        return mini;
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+       
+        // for(int i=0;i<n;i++){
+        //     int ans = solve(n-1,i,triangle,dp);
+        //     mini =  min(mini,ans);
+        // }
+        // return mini;
 
+
+        vector<vector<int>> dp(n,vector<int>(n,1e5));
+        dp[0][0]=triangle[0][0];
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i+1;j++){
+
+                int up = dp[i][j];
+                int leftup = dp[i][j];
+                if(j<i) up = dp[i-1][j]; 
+                if(j>0)  leftup = dp[i-1][j-1];
+
+                dp[i][j] = triangle[i][j] + min(up,leftup);
+                
+            }
+        }
+        int ans = INT_MAX;
+        for(int i=0;i<n;i++){
+            ans = min(ans,dp[n-1][i]);
+        }
+        return ans;
     }
 };
