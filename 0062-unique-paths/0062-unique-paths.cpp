22f86665f47eb69336127c27dbbd737bcbcbe088
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2026-02-22 21:35:11 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,76 +1,62 @@
 class Solution {
 public:
-
-    int solve(int m,int n,vector<vector<int>> &vis,vector<int> &di,vector<int> &dj,vector<vector<int>> &dp){
-
-        if(m < 0 || n< 0 ){
-            return 0;
-        }
-        if(m==0 && n==0){
+    int solve(vector<vector<int>> &dp,vector<int>&dx,vector<int>& dy,int x, int y,int m, int n){
+        if (x == m-1 && y == n-1){
             return 1;
         }
-        if(dp[m][n] != -1){
-            return dp[m][n];
+
+        if(dp[x][y]!= -1){
+            return dp[x][y];
         }
 
+        int ways = 0;
 
-        int count = 0;
 
-        for(int i=0;i<2;i++){
-            int idx = m + di[i];
-            int jdx = n + dj[i];
+        for(int i = 0;i<2;i++){
+            int ndx = x + dx[i];
+            int ndy = y + dy[i];
 
-            if( idx>=0 && jdx>=0 && !vis[idx][jdx]){
-                vis[idx][jdx] = 1;
-                count+=solve(idx,jdx,vis,di,dj,dp);
-                vis[idx][jdx] = 0;
+            if(ndx>=0 && ndx<m && ndy>=0 && ndy<n){
+                ways += solve(dp,dx,dy,ndx,ndy,m,n) ;
             }
-
         }
-        return dp[m][n] = count;
 
-
+        return dp[x][y] = ways;
     }
-
     int uniquePaths(int m, int n) {
-        // vector<vector<int>> vis(m,vector<int> (n,0));
-        // vector<int> di = {-1,0};
-        // vector<int> dj = {0,-1};
-        // vector<vector<int>> dp(m,vector<int> (n,-1));
-        // return solve(m-1,n-1,vis,di,dj,dp);
 
-        //tabulation 
-        // vector<int> di = {-1,0};
-        // vector<int> dj = {0,-1};
-        vector<vector<int>> dp(m,vector<int> (n,0));
-        dp[0][0]=1;
-        // for (int i = 1; i < m; i++) {
-        //     dp[i][0] = 1; // Only one way to reach cells in the first column
-        // }
-        // for (int j = 1; j < n; j++) {
-        //     dp[0][j] = 1; // Only one way to reach cells in the first row
-        // }
+        //Memoiation 
 
-        for( int i = 0; i < m; i++){
-            for(int j = 0;j < n; j++){
-                // int count = 0;
-                // for(int k=0;k<2;k++){
-                    // int idx = i + di[k];
-                    // int jdx = j + dj[k];
-                    // if(i == 0 && j==0) dp[i][j]=1;
-                    if (i>0) dp[i][j]+= dp[i-1][j];
-                    if (j>0) dp[i][j]+= dp[i][j-1];
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // vector<int> dx = {+1,0};
+        // vector<int> dy = {0,+1};
 
-                    // if( idx>0 && jdx>0 ){
-                    //     count+=dp[idx][jdx];
-                    // }
+        // return solve(dp,dx,dy,0,0,m,n);
 
-                // }
-                // dp[i][j]=count;
+
+        //Tabulation
+
+        vector<vector<int>> dp(m,vector<int>(n,0));
+
+        dp[0][0] = 1;
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+
+                if( i > 0 && j > 0){
+                    dp[i][j] += dp[i-1][j] + dp[i][j-1];
+                }
+                
+                else if(i > 0){
+                    dp[i][j] += dp[i-1][j] ;
+                }
+                else if(j > 0){
+                    dp[i][j] += dp[i][j-1];
+                }
+
             }
         }
+
         return dp[m-1][n-1];
-
-
     }
 };
