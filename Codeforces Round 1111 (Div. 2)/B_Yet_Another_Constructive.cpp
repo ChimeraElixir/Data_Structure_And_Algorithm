fd2f946d423137c85cec53ac26be27e5836ae873
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2026-07-20 20:58:47 +0530

    practice

--- Codeforces Round 1111 (Div. 2)/B_Yet_Another_Constructive.cpp
+++ Codeforces Round 1111 (Div. 2)/B_Yet_Another_Constructive.cpp
@@ -1,71 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
-
-using ll = long long;
-using ull = unsigned long long;
-using pii = pair<int, int>;
-using pll = pair<long long, long long>;
-using vi = vector<int>;
-using vll = vector<long long>;
-
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const long long LINF = 1e18;
-
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
-#define all(x) (x).begin(), (x).end()
-#define sz(x) (int)(x).size()
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define per(i, a, b) for (int i = (a); i >= (b); --i)
-
-#ifdef LOCAL
-    #define debug(x) cerr << #x << " = " << (x) << "\n"
-#else
-    #define debug(x)
-#endif
-
-
-
 int main() {
-    FAST_IO;
-    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
     while (t--) {
-        int n,k,m;
-        cin>>n>>k>>m;
-        vector<int> arr(n);
-        for(auto &i:arr){
-            cin>>i;
+        int n, k, m;
+        cin >> n >> k >> m;
+
+        if (k > m) {
+            cout << "NO\n";
+            continue;
         }
 
-        int start = 0 ;
-        int end = 1;
-        // int sum = 0;
-
-        int sum = accumulate(arr.begin(), arr.end(), k);
-        cout<<sum<<endl;
-        // for(int end=0;end<n;end++){
-        //     if(end-start+1<k){
-        //         sum+=arr[end];
-        //     }
-        //     else{
-        //         break;
-        //     }
-        // }
-
-        // while (end<n){
-        //     if(end-start+1 < k){
-        //         sum+= arr[end];
-        //     }
-
-        // }
+        cout << "YES\n";
+        for (int i = 0; i < n; i++) {
+            cout << (i % k == 0 ? m - k + 1 : 1) << ' ';
+        }
+        cout << '\n';
     }
-    
     return 0;
 }
