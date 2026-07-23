3967e68b18d7b90217da5520a4205eeb389326e3
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2026-07-23 13:08:23 +0530

    Practice Round 1108 div 2 and 1190 div 2
    Solved 1 problem form div 2 round 1108 and previous from 1090 both as practice

---
+++ CodeForces Round 1090 (div 2)/C_The_67_th_Permutation_Problem.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const long long LINF = 1e18;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (a); i >= (b); --i)
+
+#ifdef LOCAL
+    #define debug(x) cerr << #x << " = " << (x) << "\n"
+#else
+    #define debug(x)
+#endif
+
+
+
+int main() {
+    FAST_IO;
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n ;
+        cin>>n;
+        int x = 3 * n; 
+        int k=1;
+
+        vector<int> arr(3*n);
+
+        for(int i=0;i<n;i++){
+                arr[i*3] = k;
+                k++;
+                arr[i*3+1] = x;
+                x--;
+                arr[i*3+2] = k;
+                k++;
+        }
+        for(auto i:arr){
+            cout<<i<<' ';
+        }
+        cout<<endl;
+
+    }
+    
+    return 0;
+}
